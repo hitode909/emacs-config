70aeb77e4ff0c7c448871250fda8594bbb2d8a72
hitode909 <hitode909@gmail.com>
2013-07-20 13:49:48 +0900

    import smartchr

--- inits/80-smartchr.el
+++ inits/80-smartchr.el
@@ -1,2 +1,32 @@
+(require 'smartchr)
 (global-set-key (kbd "=") (smartchr '(" = " "=" " == " "=")))
+(global-set-key (kbd "G") (smartchr '("G" "ありがとうございます")))
 
+(add-hook 'cperl-mode-hook
+          '(lambda ()
+             (define-key cperl-mode-map (kbd ">") (smartchr '(">" " => " ">>" " > " " >= " " => '`!!''" " => \"`!!'\"")))
+             (define-key cperl-mode-map (kbd "F") (smartchr '("F" "$" "$_" "$_->" "@$")))
+             (define-key cperl-mode-map (kbd "M") (smartchr '("M" "my $`!!' = ")))
+             (define-key cperl-mode-map (kbd "D") (smartchr '("D" "use Data::Dumper; warn Dumper `!!';")))
+             (define-key cperl-mode-map (kbd "S") (smartchr '("S" "my ($self) = @_;" "my ($self, $`!!') = @_;")))
+             (define-key cperl-mode-map (kbd "C") (smartchr '("C" "my ($class) = @_;" "my ($class, $`!!') = @_;")))
+             (define-key cperl-mode-map (kbd ".") (smartchr '("->" "." "..")))
+             (define-key cperl-mode-map (kbd "|") (smartchr '(" || " "|")))
+             (define-key cperl-mode-map (kbd "&") (smartchr '(" && " "&")))
+             (define-key cperl-mode-map (kbd "{") (smartchr '("{" "sub { `!!' ")))
+             (define-key cperl-mode-map (kbd "H") (smartchr '("H" "Hatena")))
+             (define-key cperl-mode-map (kbd ":") (smartchr '("::" ": ")))
+             (define-key cperl-mode-map (kbd "E") (smartchr '("E" "Hatena::Epic::")))
+             ))
+
+(add-hook 'ruby-mode-hook
+          '(lambda ()
+             (define-key ruby-mode-map (kbd "P") (smartchr '("P" "require pp; pp ")))
+             (define-key ruby-mode-map (kbd ">") (smartchr '(">" " => " ">>" " > " " >= " " => '`!!''" " => \"`!!'\"")))
+             ))
+
+(add-hook 'espresso-mode-hook
+          '(lambda ()
+             (define-key espresso-mode-map (kbd "F") (smartchr '("F" "function() { `!!' }")))
+             (define-key espresso-mode-map (kbd "L") (smartchr '("L" "console.log( `!!' )")))
+             ))

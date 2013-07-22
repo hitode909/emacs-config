e24ef69473f480625a2e82ef4e21de983319fb7c
hitode909 <hitode909@gmail.com>
2013-07-22 16:41:39 +0900

    yas

---
+++ inits/79-yasnippet-helm.el
@@ -0,0 +1,16 @@
+;; http://d.hatena.ne.jp/syohex/20121207/1354885367
+
+(defun my-yas/prompt (prompt choices &optional display-fn)
+  (let* ((names (loop for choice in choices
+                      collect (or (and display-fn (funcall display-fn choice))
+                                  coice)))
+         (selected (helm-other-buffer
+                    `(((name . ,(format "%s" prompt))
+                       (candidates . names)
+                       (action . (("Insert snippet" . (lambda (arg) arg))))))
+                    "*helm yas/prompt*")))
+    (if selected
+        (let ((n (position selected names :test 'equal)))
+          (nth n choices))
+      (signal 'quit "user quit!"))))
+(custom-set-variables '(yas/prompt-functions '(my-yas/prompt)))

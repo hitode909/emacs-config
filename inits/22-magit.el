e7bd2417d16b5ea673f0deb3e95a9b3a4c1e15e3
hitode909 <hitode909@gmail.com>
2013-07-22 15:58:07 +0900

    git

--- inits/22-magit.el
+++ inits/22-magit.el
@@ -3,3 +3,13 @@
   (global-set-key [(super s)] 'magit-status)
   (global-set-key [(super l)] 'magit-log)
   )
+
+(setq magit-save-some-buffers nil)
+
+(setq magit-set-upstream-on-push t)
+
+(set-face-inverse-video-p 'diff-added nil)
+(set-face-inverse-video-p 'diff-removed nil)
+
+(set-face-foreground 'magit-header "#586e75")
+(set-face-background 'magit-header "#fdf6e3")

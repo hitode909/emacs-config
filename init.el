3fd2ce8806b8dbaf624e03562f674ea3cbb3c817
hitode909 <hitode909@gmail.com>
2013-07-23 09:52:12 +0900

    add init

--- init.el
+++ init.el
@@ -37,3 +37,19 @@
 (init-loader-load "~/Dropbox/private-emacs-config/inits")
 
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(helm-buffer-max-length nil)
+ '(yas/prompt-functions (quote (my-yas/prompt))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+

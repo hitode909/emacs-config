e6e0eb763efd10a6a23d5323239a2f07456dbdec
hitode909 <hitode909@gmail.com>
2013-07-29 11:09:54 +0900

    perl テスト実行

--- inits/50-perl-config.el
+++ inits/50-perl-config.el
@@ -8,6 +8,41 @@
 (setq auto-mode-alist (cons '("\\.psgi$" . cperl-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("cpanfile$" . cperl-mode) auto-mode-alist))
 
+(defun run-perl-test ()
+  "test実行します"
+  (interactive)
+  (compile (format "cd %s; carton exec -- perl %s" (vc-git-root default-directory) (buffer-file-name (current-buffer)))))
+
+(defun run-perl-method-test ()
+  (interactive)
+  (let (
+        (command compile-command)
+        (test-method nil))
+    (save-excursion
+      (when (or
+             (re-search-backward "\\bsub\s+\\([_[:alpha:]]+\\)\s*:\s*Test" nil t)
+             (re-search-forward "\\bsub\s+\\([_[:alpha:]]+\\)\s*:\s*Test" nil t))
+        (setq test-method (match-string 1))))
+    (if test-method
+        (compile (format "cd  %s; TEST_METHOD=%s carton exec -- perl %s"
+                         (vc-git-root default-directory)
+                         (shell-quote-argument test-method)
+                         (buffer-file-name (current-buffer))))
+      (let ((a 1))
+        (save-excursion
+          (when (or
+                 (re-search-backward "^subtest\s+['\"]?\\([^'\"\s]+\\)['\"]?\s*=>\s*sub" nil t)
+                 (re-search-foreward "^subtest\s+['\"]?\\([^'\"\s]+\\)['\"]?\s*=>\s*sub" nil t))
+            (setq test-method (match-string 1))))
+        (if test-method
+            (compile (format "cd  %s; SUBTEST_FILTER=%s carton exec -- perl %s"
+                             (vc-git-root default-directory)
+                             (shell-quote-argument test-method)
+                             (buffer-file-name (current-buffer)))))
+        (message "not match")
+        ))))
+
+
 (add-hook 'cperl-mode-hook
           '(lambda ()
              (setq indent-tabs-mode nil)

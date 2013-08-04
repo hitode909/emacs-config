068092aa56c509bf2d9fadebd6038b0cd0917538
hitode909 <hitode909@gmail.com>
2013-08-04 13:01:57 +0900

    use helm locate

--- inits/21-helm.el
+++ inits/21-helm.el
@@ -1,6 +1,19 @@
 (require 'helm)
 (require 'helm-config)
-(global-set-key (kbd "C-;") 'helm-mini)
+
+(defun my-helm-mini ()
+  (interactive)
+  (require 'helm-files)
+  (helm-other-buffer '(
+                       helm-source-buffers-list
+                       helm-source-files-in-current-dir
+                       helm-source-recentf
+                       helm-source-file-cache
+                       helm-source-locate
+                       )
+                     "*helm mini*"))
+
+(global-set-key (kbd "C-;") 'my-helm-mini)
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (define-key helm-map (kbd "C-h") 'delete-backward-char)

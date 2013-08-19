a9c63e80c852cdbd59d7ebe8c43f7cd85c3d6d69
hitode909 <hitode909@gmail.com>
2013-08-19 09:58:07 +0900

    js

---
+++ inits/50-js.el
@@ -0,0 +1,15 @@
+;; This function is quoted from this page. Thanks to Tomohiro Matsuyama.
+;; http://dev.ariel-networks.com/Members/matsuyama/pretty-lambda-in-emacs/
+(defun set-pretty-patterns (patterns)
+  (loop for (glyph . pairs) in patterns do
+    (loop for (regexp . major-modes) in pairs do
+      (loop for major-mode in major-modes do
+        (let ((major-mode (intern (concat (symbol-name major-mode) "-mode")))
+              (n (if (string-match "\\\\([^?]" regexp) 1 0)))
+          (font-lock-add-keywords major-mode
+                                  `((,regexp (0 (prog1 ()
+                                                       (compose-region (match-beginning ,n)
+                                                                       (match-end ,n)
+                                                                       ,glyph)))))))))))
+
+(set-pretty-patterns '((?寿 ("\\<function\\>" js))))

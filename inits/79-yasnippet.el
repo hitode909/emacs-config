ca2734dc60919864f347dc98dbea5cfd3ea1372c
hitode909 <hitode909@gmail.com>
2013-08-19 09:57:44 +0900

    yasnippet M-o

--- inits/79-yasnippet.el
+++ inits/79-yasnippet.el
@@ -1,2 +1,6 @@
 (require 'yasnippet)
 (yas-global-mode 1)
+
+;; use "M-o" to expand, not "TAB"
+(define-key yas-minor-mode-map (kbd "M-o") 'yas-expand)
+(define-key yas-minor-mode-map (kbd "TAB") nil)

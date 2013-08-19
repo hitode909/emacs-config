9cbed7c16cc09fa645ee05c8db8a93df71d1e33e
hitode909 <hitode909@gmail.com>
2013-08-19 09:57:26 +0900

    coffee

--- inits/50-coffee.el
+++ inits/50-coffee.el
@@ -1,1 +1,22 @@
-(require 'coffee-mode)
+(when (require 'coffee-mode nil t)
+  (defun coffee-custom ()
+    "coffee-mode-hook"
+    (set (make-local-variable 'tab-width) 2)
+    (setq coffee-cleanup-whitespace nil)
+    (define-key coffee-mode-map [(meta R)] 'coffee-compile-file)
+
+    ;; (add-hook 'after-save-hook
+    ;;           '(lambda ()
+    ;;              (when (string-match "\.coffee" (buffer-name)) ; ~~~.coffee<>
+    ;;                (coffee-compile-file))))
+    )
+
+  (add-hook 'coffee-mode-hook
+            '(lambda() (coffee-custom)))
+
+  (add-to-list 'auto-mode-alist '("\\.js.shd$" . coffee-mode))
+
+
+  (setq coffee-args-compile '("-cb"))
+  (setq ac-modes (cons 'coffee-mode ac-modes))
+  )

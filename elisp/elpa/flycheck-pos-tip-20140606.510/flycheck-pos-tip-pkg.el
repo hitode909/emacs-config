46c4bdbe2051095598f2ef41a828efda9da9583a
hitode909 <hitode909@gmail.com>
2015-12-08 18:01:35 +0900

    いろいろ置いとく

---
+++ elisp/elpa/flycheck-pos-tip-20140606.510/flycheck-pos-tip-pkg.el
@@ -0,0 +1,1 @@
+(define-package "flycheck-pos-tip" "20140606.510" "Flycheck errors display in tooltip" '((flycheck "0.18") (popup "0.5.0")) :url "https://github.com/flycheck/flycheck-pos-tip" :keywords '("tools" "convenience"))
